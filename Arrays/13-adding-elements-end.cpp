aea9ca901ac0c7d303753abc50aceab3186af3ae
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-09 00:24:44 +0530

    Add solution for adding an element at the end of an array

---
+++ Arrays/13-adding-elements-end.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insertatEnd(int *arr, int n, int value)
+{
+    arr[n] = value;
+}
+
+int main()
+{
+    int n=8;
+    int arr[9]={10,9,14,8,20,48,16,9};
+    int value=40;
+    cout<<"Before inserting the value at beginning:"<<endl;
+     for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    insertatEnd(arr,n,value);
+    cout<<"After inserting the value at beginning:"<<endl;
+    for(int i=0;i<=n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
