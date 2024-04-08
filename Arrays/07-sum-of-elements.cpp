0ce4d294c8ac4a126d84482f7b27a017a161b2e6
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-08 10:24:06 +0530

    Add solution for finding the sum of elements in an array

---
+++ Arrays/07-sum-of-elements.cpp
@@ -0,0 +1,16 @@
+// Problem Statement: Given an array, we have to find the sum of all the elements in the array.
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int sum = 0;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+    }
+    cout << sum;
+}
