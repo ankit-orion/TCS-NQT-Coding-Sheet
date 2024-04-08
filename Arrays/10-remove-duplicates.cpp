c79c2460e6ec31cb8c6e07759cf1fa79e1e642ef
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-08 18:53:45 +0530

    Refactor code to remove duplicates from an integer array(optimised)

--- Arrays/10-remove-duplicates.cpp
+++ Arrays/10-remove-duplicates.cpp
@@ -3,6 +3,22 @@
 // The relative order of the elements should be kept the same.
 #include<bits/stdc++.h>
 using namespace std;
+// int main(){
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for(int i = 0; i < n; i++){
+//         cin >> arr[i];
+//     }
+//     set<int> s;
+//     for(int i = 0; i < n; i++){
+//         s.insert(arr[i]);
+//     }
+//     for(auto it: s){
+//         cout << it << " ";
+//     }
+//     return 0;
+// }
 int main(){
     int n;
     cin >> n;
@@ -10,12 +26,15 @@ int main(){
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    set<int> s;
-    for(int i = 0; i < n; i++){
-        s.insert(arr[i]);
+    int j = 0;
+    for(int i =1; i < n; i++){
+        if(arr[i] != arr[j]){
+            j++;
+            arr[j] = arr[i];
+        }
     }
-    for(auto it: s){
-        cout << it << " ";
+    for(int i = 0; i < j+1; i++){
+        cout << arr[i] << " ";
     }
     return 0;
 }
