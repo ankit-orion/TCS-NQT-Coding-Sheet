6087deb52d239ac17bbe316c74f12c991573600a
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-13 22:04:04 +0530

    Add code for checking perfect numbers

---
+++ Numbers/06-check-for-perfect-number.cpp
@@ -0,0 +1,3 @@
+// Problem Statement: Perfect Number. Write a program to find whether a number is a perfect number or not.
+
+// A perfect number is defined as a number that is the sum of its proper divisors ( all its positive divisors excluding itself). 
