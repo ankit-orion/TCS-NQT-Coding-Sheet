305f3b52b416b1a2dd8a214878d184789dea83b6
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-17 00:17:59 +0530

    Add code for finding the ASCII value of a character in 03-char-to-ascii.cpp

---
+++ Strings/03-char-to-ascii.cpp
@@ -0,0 +1,9 @@
+// Problem Statement: Given a character, Find the ASCII value of the character.
+#include<iostream>
+using namespace std;
+int main(){
+    char c;
+    cin >> c;
+    cout << int(c) << endl;
+    return 0;
+}
