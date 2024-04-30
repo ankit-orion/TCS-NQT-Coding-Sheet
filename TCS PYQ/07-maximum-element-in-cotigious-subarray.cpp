1a9a0c84f3718bac3eb69a0700cc98b517b4098f
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-04-30 23:42:16 +0530

    Add code for finding the maximum element in contiguous subarrays

---
+++ TCS PYQ/07-maximum-element-in-cotigious-subarray.cpp
@@ -0,0 +1,30 @@
+// given an array and a integer k. we need to find the maximum element
+// in each of the contiguous subarrays
+// Input: 2 4 7 1 6 3
+// k = 3
+// Output: 7 7 7 6
+// The subarrays will be be {2, 4, 7}, {4, 7, 1}, {7, 1, 6}, {1, 6, 3}
+// The maximum element in each of the subarrays are 7, 7, 7, 6
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i =0; i < n; i++){
+        cin >> arr[i];
+    }
+    int k;
+    cin >> k;
+    for(int i = 0; i <= n - k; i ++){
+        int maxi = INT_MIN;
+        // for every subarray starting from i I've to find out the maximum
+        for(int j = i; j < i + k; j++){
+            // printing the cuttrrent subarray
+            cout << arr[j] << " ";
+            maxi= max(arr[j], maxi);
+        }
+        cout <<"Maximum element from the subarray " << maxi << endl;
+    }
+    return 0;
+}
