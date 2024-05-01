070d0b7cbad65f476be5501ce7cdcb3b9a71cec5
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-01 10:38:29 +0530

    Add code for finding the maximum product subarray (brute force approach TC: O(n^2) SC: O(1) no extra space required

---
+++ TCS PYQ/09-max-product-subarray.cpp
@@ -0,0 +1,24 @@
+// Given an array that contains both negative and positive integers, find the maximum product subarray.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    int maxi = INT_MIN;
+    // brute force approach could be to find out all the subarrays
+    // and then we can store it's product in a variable at each step we can compare with the previpis one
+    for(int i = 0; i < n;i++){
+        int product = 1;
+        for(int j = i; j < n; j++){
+            product *= arr[j];
+            maxi = max(maxi, product);
+        }
+    }
+    cout << maxi << endl;
+    // time complexity will be O(n^2);
+    // space complexity will be O(1); as no extra space requried
+}
