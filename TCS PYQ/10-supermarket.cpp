263a690852b46df2be6f5803573a67b1cb2090e5
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-05 22:49:16 +0530

    feat: Add code for computing the price of an item based on its value

---
+++ TCS PYQ/10-supermarket.cpp
@@ -0,0 +1,17 @@
+// 1. A supermarket maintains a pricing format for all its products. A value N printed on each product. When 
+// the scanner reads the value N on the item, the product of all the digits in the value N is the price of the item. 
+// The task is to design a software such that given the code of any item N the product(multiplication) of all the 
+// digits of value should be computed(price)
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int price = 1;
+    while(n){
+        int digit = n%10;
+        price *= digit;
+        n /= 10;
+    }
+    cout << price << endl;
+}
