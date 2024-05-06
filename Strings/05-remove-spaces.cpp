7949ac5c473ad634e59983f8e8076d2bfd2386e7
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-05-06 23:37:51 +0530

    Refactor code to remove spaces from a string

--- Strings/05-remove-spaces.cpp
+++ Strings/05-remove-spaces.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main(){
     string s;
     s = "My name is Ankit Mishra";
+    // s.erase(remove(s.begin(), s.end(), ' '), s.end());
+    // cout << s << endl;
     for(int i = 0; i < s.length(); i++){
         if(s[i] == ' '){
             s.erase(i, 1);
             i--;
         }
     }
+    // s.erase(remove(s.begin(), s.end(), ' '), s.end());
     cout << s << endl;
 }
