c9f75898af93b72c44601dd6a23398d486be53d3
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2024-06-28 16:31:48 +0530

    refactor: Implement merge sort algorithm

---
+++ Sorting/mergeSort.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void merge(int arr[], int low, int mid, int high){
+    vector<int> temp;
+    int left = low;
+    int right = mid+1;
+    while(left <= mid && right <= high){
+        if(arr[left] < arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i = low; i <= high; i++){
+        arr[i] = temp[i-low];
+    }
+
+}
+
+void mergeSort(int arr[], int low, int high){
+    if(low == high){
+        return;
+    }
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+int main(){
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    cout << "The elements before sorting are: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    // Merge Sort
+    mergeSort(arr, 0, n-1);
+    cout << "The elements after sorting are: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
