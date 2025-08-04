5c3010befc2e50d9c5464edb538bd7908eec586c
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2025-08-05 00:43:36 +0530

    Updated Array Questions

--- Arrays/01-smallestNumber.cpp
+++ Arrays/01-smallestNumber.cpp
@@ -1,30 +1,31 @@
-#include<iostream>
-#include<algorithm>
+// Problem Statement: Given an array, we have to find the smallest element in the array.
+
+// Examples:
+
+// Example 1:
+// Input: arr[] = {2,5,1,3,0};
+// Output: 0
+// Explanation: 0 is the smallest element in the array. 
+
+// Example2: 
+// Input: arr[] = {8,10,5,7,9};
+// Output: 5
+// Explanation: 5 is the smallest element in the array.
+
+#include<bits/stdc++.h>
 using namespace std;
-//1st Program using sorting TC: o(N*log(N)), sc(1)
-int main(){
-    int n;
-    cin >> n;
-    int arr[n];
-    for(int i = 0; i < n; i++){
-        cin >> arr[i];
-    }
-    sort(arr, arr+n);
-    cout <<"smallest number is "<< arr[0] << endl;
+ 
+int sortArr(vector<int>& arr) {
+    sort(arr.begin(),arr.end());
+    return arr[0];
+}
+ 
+int main() {
+    vector<int> arr1 = {2,5,1,3,0};
+    vector<int> arr2 = {8,10,5,7,9};
+   
+    cout<<"The smallest element in the array is: "<<sortArr(arr1)<<endl;
+    cout<<"The smallest element in the array is: "<<sortArr(arr2);
+   
     return 0;
 }
-// 1st program TC:o(N), SC: o(1)
-// int main(){
-//     int n;
-//     cin >> n;
-//     int arr[n];
-//     for(int i = 0; i < n; i++){
-//         cin >> arr[i];
-//     }
-//     int num = arr[0];
-//     for(int i = 1; i < n; i++){
-//         if(arr[i] < num) num = arr[i];
-//     }
-//     cout <<"smallest number is "<< num << endl;
-//     return 0;
-// }
