ee3623462a517a5e250135aaf83643919cdec2c8
Ankit Mishra <72105760+ankit-orion@users.noreply.github.com>
2025-08-05 00:47:22 +0530

    Checking md file

--- Arrays/01-smallestNumber.cpp
+++ Arrays/01-smallestNumber.cpp
@@ -12,6 +12,11 @@
 // Output: 5
 // Explanation: 5 is the smallest element in the array.
 
+// Solution 1: Using Sorting
+// We can sort the array in ascending order, hence the smallest element will be at the 0th index. 
+
+// Code:
+
 #include<bits/stdc++.h>
 using namespace std;
  
@@ -29,3 +34,32 @@ int main() {
    
     return 0;
 }
+
+// Solution 2: Using a min Variable
+
+// #include <iostream>
+// #include <bits/stdc++.h>
+ 
+// using namespace std;
+// int SmallestElement(int arr[], int n) {
+ 
+//   int min = arr[0];
+//   for (int i = 0; i < n; i++) {
+//     if (min > arr[i]) {
+//       min = arr[i];
+//     }
+//   }
+//   return min;
+// }
+// int main() {
+//   int arr1[] = {2,5,1,3,0};
+//   int n = 5;
+//   int min = SmallestElement(arr1, n);
+//   cout << "The smallest element in the array is: " << min << endl;
+ 
+//   int arr2[] =  {8,10,5,7,9};
+//   n = 5;
+//   min = SmallestElement(arr2, n);
+//   cout << "The smallest element in the array is: " << min;
+//   return 0;
+// }
